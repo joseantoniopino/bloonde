f74a411009704102bfff55a7326856402c425a54
Jose Pino <josanpino@gmail.com>
2024-07-10 04:42:26 +0200

    feat(auth) add auth system

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginAuthRequest;
+use App\Http\Requests\RegisterAuthRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * Register a new user.
+     */
+    public function register(RegisterAuthRequest $request)
+    {
+
+        $user = User::create([
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'profile_id' => $request->profile_id,
+            'status_id' => $request->status_id,
+        ]);
+
+        return response()->json(['user' => $user, 'message' => 'User registered successfully'], 201);
+    }
+
+    /**
+     * Login a user and create token.
+     */
+    public function login(LoginAuthRequest $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['token' => $token, 'token_type' => 'Bearer']);
+    }
+
+    /**
+     * Logout user (Revoke the token).
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Token revoked']);
+    }
+}
